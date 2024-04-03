f4c25da5d44b3ef781e2d65461b7a183b82872f5
Deep Modiya <202001433@daiict.ac.in>
2024-04-03 23:35:45 +0530

    api logic of add-to-cart

--- src/app/api/cart/add-to-cart/route.js
+++ src/app/api/cart/add-to-cart/route.js
@@ -1,67 +1,78 @@
 import connectToDB from "@/database";
-import AuthUser from "@/middeware/AuthUser";
+import AuthUser from "@/middleware/AuthUser";
 import Cart from "@/models/cart";
-import { NextResponse } from "next/server";
 import Joi from "joi";
+import { NextResponse } from "next/server";
+
+const AddToCart = Joi.object({
+  userID: Joi.string().required(),
+  productID: Joi.string().required(),
+});
+
+export const dynamic = "force-dynamic";
 
 export async function POST(req) {
-    try{
-        await connectToDB();
+  try {
+    await connectToDB();
+    const isAuthUser = await AuthUser(req);
 
-        const isAuthUser = await AuthUser(req);
+    if (isAuthUser) {
+      const data = await req.json();
+      const {productID , userID} = data;
 
-        if(isAuthUser){
+      const { error } = AddToCart.validate({ userID, productID });
 
-            const data = await req.json();
-            const {productID,  userID} = data;
-
-            const {error} = addToCart.validate({userID, productID});
-
-            if(error){
-                return NextResponse.json({
-                    success: false,
-                    message: error.details[0].message,
-                  });
-            } 
-
-            const isCurrentCartItemAlreadyExists = await Cart.find({
-                productID : productID,
-                userID : userID
-            })
-
-            if(isCurrentCartItemAlreadyExists) {
-                return NextResponse.json({
-                    success: false,
-                    message: "product is already in the cart",
-                  });
-            }
-
-            const saveProductToCart = await Cart.create(data);
-
-            if(saveProductToCart){
-                return NextResponse.json({
-                    success: true,
-                    message: "Product is added to cart!",
-                  }); 
-            } else {
-                return NextResponse.json({
-                    success: false,
-                    message: "failed to add the product to cart !",
-                  });
-            }
-
-        }else {
-            return NextResponse.json({
-                success: false,
-                message: "You are not authenticated",
-              });
-        }
-        
-    }catch(e){
-        console.log(error);
+      if (error) {
         return NextResponse.json({
           success: false,
-          message: "Something went wrong ! Please try again later",
+          message: error.details[0].message,
         });
+      }
+
+      console.log(productID, userID);
+
+      const isCurrentCartItemAlreadyExists = await Cart.find({
+        productID: productID,
+        userID: userID,
+      });
+
+      console.log(isCurrentCartItemAlreadyExists);
+      
+
+      if (isCurrentCartItemAlreadyExists?.length > 0) {
+        return NextResponse.json({
+          success: false,
+          message:
+            "Product is already added in cart! Please add different product",
+        });
+      }
+
+      const saveProductToCart = await Cart.create(data);
+
+      console.log(saveProductToCart);
+
+      if (saveProductToCart) {
+        return NextResponse.json({
+          success: true,
+          message: "Product is added to cart !",
+        });
+      } else {
+        return NextResponse.json({
+          success: false,
+          message: "failed to add the product to cart ! Please try again.",
+        });
+      }
+    } else {
+      return NextResponse.json({
+        success: false,
+        message: "You are not authenticated",
+      });
     }
-};
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json({
+      success: false,
+      message: "Something went wrong ! Please try again later",
+    });
+  }
+}

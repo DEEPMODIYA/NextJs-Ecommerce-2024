dd280e5eeebef2b00545ca2ba8f476b4ce9d7d1e
Deep Modiya <202001433@daiict.ac.in>
2024-04-08 12:21:01 +0530

    adding account page form details

--- src/utils/index.js
+++ src/utils/index.js
@@ -199,4 +199,42 @@ export const navOptions = [
   };
 
   export const firebaseStorageURL = 'gs://next-js-2024.appspot.com';
+
+  export const addNewAddressFormControls = [
+    {
+      id: "fullName",
+      type: "input",
+      placeholder: "Enter your full name",
+      label: "Full Name",
+      componentType: "input",
+    },
+    {
+      id: "address",
+      type: "input",
+      placeholder: "Enter your full address",
+      label: "Address",
+      componentType: "input",
+    },
+    {
+      id: "city",
+      type: "input",
+      placeholder: "Enter your city",
+      label: "City",
+      componentType: "input",
+    },
+    {
+      id: "country",
+      type: "input",
+      placeholder: "Enter your country",
+      label: "Country",
+      componentType: "input",
+    },
+    {
+      id: "postalCode",
+      type: "input",
+      placeholder: "Enter your postal code",
+      label: "Postal Code",
+      componentType: "input",
+    },
+  ];
   

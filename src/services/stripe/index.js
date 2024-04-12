834812a5e2b4f58aabd916c7c77d393c9601a0c6
Deep Modiya <202001433@daiict.ac.in>
2024-04-12 20:54:45 +0530

    changes in services stripe

--- src/services/stripe/index.js
+++ src/services/stripe/index.js
@@ -1,21 +1,20 @@
 import Cookies from "js-cookie";
 
-
 export const callStripeSession = async (formData) => {
-    try {
-        const res = await fetch("/api/stripe", {
-            method : "POST",
-            headers : {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${Cookies.get("token")}`,
-            },
-            body: JSON.stringify(formData),
-        });
+  try {
+    const res = await fetch("/api/stripe", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${Cookies.get("token")}`,
+      },
+      body: JSON.stringify(formData),
+    });
 
-        const data = await res.json();
-        return data;
+    const data = await res.json();
 
-    } catch (e) {
-        console.log(e);
-    }
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
 };

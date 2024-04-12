67799e613c63c0be776d1a27894afe5ae3ef7b40
Deep Modiya <202001433@daiict.ac.in>
2024-04-12 20:53:42 +0530

    changes in api route

--- src/app/api/stripe/route.js
+++ src/app/api/stripe/route.js
@@ -1,43 +1,42 @@
 import AuthUser from "@/middleware/AuthUser";
 import { NextResponse } from "next/server";
 
-const stripe = require("stripe")("sk_test_51P3X8iSIc5ODEV6brIzslAZXVWjw9gqEe10fsOjHrvcJVoOoEXCiUyTMXTYrcSiiP97lnyzn7vfNx18IOzrZQHpK006SjSI5VH");
+const stripe = require("stripe")(
+  "sk_test_51P3X8iSIc5ODEV6brIzslAZXVWjw9gqEe10fsOjHrvcJVoOoEXCiUyTMXTYrcSiiP97lnyzn7vfNx18IOzrZQHpK006SjSI5VH"
+);
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
-export async function POST(req){
-    try {
+export async function POST(req) {
+  try {
+    const isAuthUser = await AuthUser(req);
+    if (isAuthUser) {
+      const res = await req.json();
 
-        const isAuthUser = await AuthUser(req);
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items: res,
+        mode: "payment",
+        success_url: "http://localhost:3000/checkout" + "?status=success",
+        cancel_url: "http://localhost:3000/checkout" + "?status=cancel",
+      });
 
-        if(isAuthUser) {
-            const res = await req.json();
-
-            const session = await stripe.checkout.sessions.create({
-                payment_method_types : ["card"],
-                line_items : res,
-                mode : 'payment',
-                success_url : 'http://localhost:3000/checkout' + '?status=success',
-                cancel_url : 'http://localhost:3000/checkout' + '?status=cancel',
-            });
-    
-            return NextResponse.json({
-                success : true,
-                id : session.id,
-            });
-    
-        } else {
-            return NextResponse.json({
-                success : false,
-                message : 'You are not Authenticated'
-            });
-        }
-    }catch(e){
-        console.log(e);
-        return NextResponse.json({
-            status : 500,
-            success : false,
-            message : 'Something went wrong ! Please try again'
-        });
+      return NextResponse.json({
+        success: true,
+        id: session.id,
+      });
+    } else {
+      return NextResponse.json({
+        success: true,
+        message: "You are not authenticated",
+      });
     }
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json({
+      status: 500,
+      success: false,
+      message: "Something went wrong ! Please try again",
+    });
+  }
 }

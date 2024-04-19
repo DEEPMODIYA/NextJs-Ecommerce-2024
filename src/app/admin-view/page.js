49d4b1eca62e8335ebb5a6bdb61541a089179004
Deep Modiya <202001433@daiict.ac.in>
2024-04-19 22:59:59 +0530

    create adminview page

--- src/app/admin-view/page.js
+++ src/app/admin-view/page.js
@@ -1,7 +1,24 @@
+'use client'
 
+import { GlobalContext } from "@/context"
+import { getAllOrdersForAllUsers } from "@/services/order";
+import { useContext, useEffect } from "react"
 
 
 export default function AdminView() {
+
+    const { user,allOrdersForAllUsers, setAllOrdersForAllUsers } = useContext(GlobalContext);
+
+    async function extractAllOrdersForAllUsers() {
+        const res = await getAllOrdersForAllUsers();
+
+        console.log(res);
+    }
+
+    useEffect(() => {
+        if (user !== null) extractAllOrdersForAllUsers();
+    },[user])
+
     return (
         <div>Admin view</div>
     )

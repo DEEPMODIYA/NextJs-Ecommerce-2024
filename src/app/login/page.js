8b28d6aacf2e6e55663e6a4ec48831aade1e3bf4
Deep Modiya <202001433@daiict.ac.in>
2024-04-26 13:06:12 +0530

    html changes

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -133,7 +133,7 @@ export default function Login() {
                  {/* Login */}
                 </button>
                 <div className="flex flex-col gap-2">
-                  <p>New to website re?</p>
+                  <p>New to website ?</p>
                   <button
                     className="inline-flex w-full items-center justify-center bg-black px-6 py-4 text-lg 
                      text-white transition-all duration-200 ease-in-out focus:shadow font-medium uppercase tracking-wide
